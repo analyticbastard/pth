a70cfdb9a7f5b2f25ef625ab42dfb316e4fc698a
analyticbastard <javierarrieropais@outlook.com>
2015-10-10 20:56:35 +0200

    Functional tests

--- test/pth_test.clj
+++ test/pth_test.clj
@@ -5,8 +5,29 @@
 (deftest test-with-no-forms
   (testing "Testing evaluation with no forms"
     (let [input-value 1]
-      (is (= (-< input-value) input-value))
-      (is (= (-<< input-value) input-value)))))
+      (is (= (-< input-value) [input-value]))
+      (is (= (-<< input-value) [input-value])))))
+
+(deftest test-pre-thread
+  (testing "Testing evaluation with no forms"
+    (let [input-value :1
+          my-map1 {:1 1, :2 2, :3 3}
+          my-map2 {:1 5, :2 6, :3 7}]
+      (is (= (-<< input-value
+                  identity
+                  my-map1
+                  my-map2)
+             [input-value (my-map1 input-value) (my-map2 input-value)])))))
+
+(deftest test-post-thread
+  (testing "Testing evaluation with no forms"
+    (let [input-value [:1 :2]
+          my-map1 {:1 1, :2 2, :3 3}
+          my-map2 {:1 5, :2 6, :3 7}]
+      (is (= (-<< input-value
+                  (map my-map1)
+                  (map my-map2))
+             ['(1 2) '(5 6)])))))
 
 
 (comment
