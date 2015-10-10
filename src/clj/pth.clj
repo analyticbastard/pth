95ce48a3de68c7d064e7d0e24022cc530544f9c8
analyticbastard <javierarrieropais@outlook.com>
2015-10-10 20:39:04 +0200

    Added macros

--- src/clj/pth.clj
+++ src/clj/pth.clj
@@ -1,7 +1,23 @@
 (ns pth)
 
-(defmacro -< [x]
-  x)
+(defmacro -< [x & forms]
+  (loop [prev (if-not forms [x] [])
+         forms forms]
+    (if forms
+      (let [form (first forms)
+            current (if (seq? form)
+                       (with-meta `(~(first form) ~x ~@(next form)) (meta form))
+                       (list form x))]
+        (recur (conj prev current) (next forms)))
+      prev)))
 
-(defmacro -<< [x]
-  x)
+(defmacro -<< [x & forms]
+  (loop [prev (if-not forms [x] [])
+         forms forms]
+    (if forms
+      (let [form (first forms)
+            current (if (seq? form)
+                      (with-meta `(~(first form) ~@(next form) ~x) (meta form))
+                      (list form x))]
+        (recur (conj prev current) (next forms)))
+      prev)))
