4a36ad8b7cb4dadf7c3a9a6fc1c05ba3b455d565
analyticbastard <javierarrieropais@outlook.com>
2015-10-27 22:27:00 +0100

    Tests for placeholders and parflows used within Clojure threads

--- test/pth_test.clj
+++ test/pth_test.clj
@@ -19,6 +19,29 @@
                  my-map2)
              [input-value (my-map1 input-value) (my-map2 input-value)])))))
 
+(deftest test-pre-thread-inside-flow
+  (testing "Testing pre-par-flow inside Clojure flow"
+    (let [input-value [:1 :2]
+          my-map1 {:1 1, :2 2, :3 3}
+          my-map2 {:1 5, :2 6, :3 7}]
+      (is (= (-> input-value
+                 (-< identity
+                     my-map1
+                     my-map2))
+             [input-value (my-map1 input-value) (my-map2 input-value)])))))
+
+(deftest test-pre-thread-inside-flow-placeholder
+  (testing "Testing pre-par-flow inside Clojure flow with placeholder"
+    (let [input-value [:1 :2]
+          my-map1 {:1 1, :2 2, :3 3}
+          my-map2 {:1 5, :2 6, :3 7}]
+      (is (= (->> input-value
+                  (-< identity
+                      my-map1
+                      my-map2
+                      :_))
+             [input-value (my-map1 input-value) (my-map2 input-value)])))))
+
 (deftest test-post-thread
   (testing "Testing evaluation with no forms"
     (let [input-value [:1 :2]
@@ -29,8 +52,18 @@
                   input-value)
              ['(1 2) '(5 6)])))))
 
-(deftest test-post-thread-placeholder
-  (testing "Testing placeholder"
+(deftest test-post-thread-inside-flow
+  (testing "Testing post-par-flow inside Clojude flow"
+    (let [input-value [:1 :2]
+          my-map1 {:1 1, :2 2, :3 3}
+          my-map2 {:1 5, :2 6, :3 7}]
+      (is (= (->> input-value
+                  (-<< (map my-map1)
+                       (map my-map2)))
+             ['(1 2) '(5 6)])))))
+
+(deftest test-post-thread-inside-flow-placeholder
+  (testing "Testing post-par-flow inside Clojude flow with placeholder"
     (let [input-value [:1 :2]
           my-map1 {:1 1, :2 2, :3 3}
           my-map2 {:1 5, :2 6, :3 7}]
