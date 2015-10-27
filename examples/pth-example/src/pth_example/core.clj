7ba5a6c512cf4735d642f5cbe89d2f080527802d
analyticbastard <javierarrieropais@outlook.com>
2015-10-27 22:52:19 +0100

    Example updated

--- examples/pth-example/src/pth_example/core.clj
+++ examples/pth-example/src/pth_example/core.clj
@@ -6,12 +6,10 @@
   [& args]
   (let [map1 {:1 :a :2 :b :3 :c}
         map2 {:1 1 :2 2 :3 3}
-        combine (partial apply interleave)
-        into-map (partial apply array-map)
         ]
-    (-> [:1 :2]
-        (-<< (map map1)
-             (map map2))
-        combine
-        into-map
-        )))
+    (->> [:1 :2]
+         (-<< (map map1)
+              (map map2))
+         (apply interleave)
+         (apply hash-map)
+         )))
