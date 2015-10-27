ab77db2ac8609dacac64a4e77674b9d3e5bcedd9
analyticbastard <javierarrieropais@outlook.com>
2015-10-27 22:25:34 +0100

    Implemented placeholder for pre-threads

--- src/clj/pth.clj
+++ src/clj/pth.clj
@@ -1,15 +1,18 @@
 (ns pth)
 
-(defmacro -< [x & forms]
-  (loop [prev (if-not forms [x] [])
-         forms forms]
-    (if forms
-      (let [form (first forms)
-            current (if (seq? form)
-                       (with-meta `(~(first form) ~x ~@(next form)) (meta form))
-                       (list form x))]
-        (recur (conj prev current) (next forms)))
-      prev)))
+(defmacro -< [& forms]
+  (let [placeholder (-> forms butlast last)
+        [x forms] (if (= :_ placeholder) [(last forms) (-> forms butlast butlast)]
+                                         [(first forms) (next forms)])]
+    (loop [prev (if-not forms [x] [])
+           forms forms]
+      (if forms
+        (let [form (first forms)
+              current (if (seq? form)
+                        (with-meta `(~(first form) ~x ~@(next form)) (meta form))
+                        (list form x))]
+          (recur (conj prev current) (next forms)))
+        prev))))
 
 (defmacro -<< [& forms]
   (let [placeholder (second forms)
